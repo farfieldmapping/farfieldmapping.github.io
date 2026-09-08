b48011872a6cb6f6cbae5460278604e23e40f920
farfieldmapping <farfieldmapping@gmail.com>
2026-09-08 15:31:32 -0400

    Add files via upload

---
+++ data/Places.js
@@ -0,0 +1,30 @@
+var json_Places = {
+"type": "FeatureCollection",
+"name": "Places",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "Name": "44 Sheridan", "Type": "House", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.071791343534827, 39.944104675631209 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "Name": "Casino Pier", "Type": "Attraction", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.069829497889117, 39.942742309414584 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "Name": "Pool", "Type": "Amenity", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.071723692153327, 39.94293745806673 ] } },
+{ "type": "Feature", "properties": { "fid": 4, "Name": "Funtown", "Type": "Attraction", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.071322622948742, 39.937011503621115 ] } },
+{ "type": "Feature", "properties": { "fid": 5, "Name": "Carousel", "Type": "Attraction", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.070577993977849, 39.942818652331425 ] } },
+{ "type": "Feature", "properties": { "fid": 6, "Name": "Good Time Charlies", "Type": "Bar", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.07301360181927, 39.943920765584423 ] } },
+{ "type": "Feature", "properties": { "fid": 7, "Name": "912 Boulevard", "Type": "Building", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.073046026548738, 39.943688707708347 ] } },
+{ "type": "Feature", "properties": { "fid": 8, "Name": "Seaside Heights Pharmacy", "Type": "Pharmacy", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.073550131539875, 39.940760108382591 ] } },
+{ "type": "Feature", "properties": { "fid": 9, "Name": "Seaside Heights Elementary School", "Type": "School", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.07117063188052, 39.943738260454879 ] } },
+{ "type": "Feature", "properties": { "fid": 10, "Name": "Ice cream place", "Type": "Restaurant", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.073488942733817, 39.944306549376094 ] } },
+{ "type": "Feature", "properties": { "fid": 11, "Name": "Paruta's", "Type": "Store", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.072868057802964, 39.945298232887353 ] } },
+{ "type": "Feature", "properties": { "fid": 12, "Name": "Fire Station", "Type": "Fire Station", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.074049867602568, 39.943638262172215 ] } },
+{ "type": "Feature", "properties": { "fid": 13, "Name": "Our Lady of Perpetual Help", "Type": "Church", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.074055796344112, 39.942998282996243 ] } },
+{ "type": "Feature", "properties": { "fid": 14, "Name": "Seaside Heights Water Tower", "Type": "Water Tower", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.078656977430242, 39.943768865858431 ] } },
+{ "type": "Feature", "properties": { "fid": 15, "Name": "ACME or A&P", "Type": "Grocery Store", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.076267243144571, 39.950734667759363 ] } },
+{ "type": "Feature", "properties": { "fid": 16, "Name": "The Wheel", "Type": "Restaurant", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.077654265955999, 39.94939775368065 ] } },
+{ "type": "Feature", "properties": { "fid": 17, "Name": "Flo's Drive-In", "Type": "Restaurant", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.072148240428803, 39.948895014222039 ] } },
+{ "type": "Feature", "properties": { "fid": 18, "Name": "Hershey Motel", "Type": "Lodging", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.073397955508952, 39.947214662801095 ] } },
+{ "type": "Feature", "properties": { "fid": 19, "Name": "Parrot Club", "Type": "Bar", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.069974162687856, 39.942963058658208 ] } },
+{ "type": "Feature", "properties": { "fid": 20, "Name": "Barney's Fish & Chips", "Type": "Restaurant", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.074126372072527, 39.937734783946418 ] } },
+{ "type": "Feature", "properties": { "fid": 21, "Name": "Chatterbox", "Type": "Bar", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.071596635398137, 39.938478100760257 ] } },
+{ "type": "Feature", "properties": { "fid": 22, "Name": "Barnegat Ice Plant", "Type": "Store", "Established": null, "Closed": null }, "geometry": { "type": "Point", "coordinates": [ -74.075360125250882, 39.943333241135946 ] } }
+]
+}
+
