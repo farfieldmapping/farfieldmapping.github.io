d5fd1fb427fce482fe2a39143c4538956115fe8e
farfieldmapping <farfieldmapping@gmail.com>
2026-09-12 17:29:02 -0400

    edits

---
+++ data/Municipal_labels.js
@@ -0,0 +1,11 @@
+var json_municipal_labels = {
+"type": "FeatureCollection",
+"name": "Places",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "Name": "Seaside Hights"}, "geometry": { "type": "Point", "coordinates": [-74.07689, 39.94115] } },
+{ "type": "Feature", "properties": { "fid": 2, "Name": "Seaside Park"}, "geometry": { "type": "Point", "coordinates": [-74.077558, 39.935589] } },
+{ "type": "Feature", "properties": { "fid": 3, "Name": "Ortley Beach"}, "geometry": { "type": "Point", "coordinates": [-74.077326, 39.952517] } }
+
+]
+}
